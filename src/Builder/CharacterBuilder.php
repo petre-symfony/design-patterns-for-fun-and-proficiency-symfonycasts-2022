26d83104818db95ae58ce96f9723dc02d7a8b117
petrero <petrero@gmail.com>
2023-01-06 21:45:33 +0200

    4.2 The Creational Method  - The final method that our builder needs is the one that will actually build the Character. You can call this anything you want, how about buildCharacter(). And it is, of course, going to return a Character object.
     To store the character stats, we're going to create four properties, which I'll paste in: private int $maxHealth, private int $baseDamage, and then private string $attackType and private string $armorType. Then, in each method, assign that property and return $this. We'll do that for $baseDamage... $attackType... and $armorType.
    
     Beautiful! The buildCharacter() method is fairly straightforward: we do whatever ugly work needed to create the Character. So I'll say return new Character() passing $this->maxHealth and $this->baseDamage. The last two arguments require objects... so they're a bit more complex. But that's ok! I don't mind if my builder gets a little complicated.
    
     Doing some Heavy Lifting
     - I'll go to the bottom of this class and paste in two new private methods. These handle creating the AttackType and ArmorType objects. Except... I need a bunch of use statements for this, which I forgot. Whoops. So I'm going to re-type the end of these classes and hit "tab" to add those use statements. There we go!
    
     Okay, we can now use the two new private methods to map the strings to objects. This is the heavy lifting - and the real value - of CharacterBuilder. Say $this->createAttackType() and $this->createArmorType().
    
     And... our builder is done! Next: let's use this in GameApplication. Then, we'll make our builder even more flexible (but not more difficult to use) by accounting for characters that use multiple attack types.

--- src/Builder/CharacterBuilder.php
+++ src/Builder/CharacterBuilder.php
@@ -2,23 +2,70 @@
 
 namespace App\Builder;
 
+use App\ArmorType\ArmorType;
+use App\ArmorType\IceBlockType;
+use App\ArmorType\LeatherArmorType;
+use App\ArmorType\ShieldType;
+use App\AttackType\AttackType;
+use App\AttackType\BowType;
+use App\AttackType\FireboltType;
+use App\AttackType\TwoHandedSwordType;
 use App\Character\Character;
 
 class CharacterBuilder {
+	private int $maxHealth;
+	private int $baseDamage;
+	private string $attackType;
+	private string $armorType;
 
-	public function setMaxHealth(int $maxHealth): self{
+	public function setMaxHealth(int $maxHealth): self {
+		$this->maxHealth = $maxHealth;
 
+		return $this;
 	}
 
-	public function setBaseDamage(int $baseDamage): self{
+	public function setBaseDamage(int $baseDamage): self {
+		$this->baseDamage = $baseDamage;
 
+		return $this;
 	}
 
-	public function setAttackType(string $attackType): self{
+	public function setAttackType(string $attackType): self {
+		$this->attackType = $attackType;
 
+		return $this;
 	}
 
-	public function setArmorType(string $armorType): self{
+	public function setArmorType(string $armorType): self {
+		$this->armorType = $armorType;
 
+		return $this;
+	}
+
+	public function buildCharacter(): Character {
+		return new Character(
+			$this->maxHealth,
+			$this->baseDamage,
+			$this->createArmorType(),
+			$this->createAttackType()
+		);
+	}
+
+	private function createAttackType(): AttackType {
+		return match ($this->attackType) {
+			'bow' => new BowType(),
+			'fire_bolt' => new FireboltType(),
+			'sword' => new TwoHandedSwordType(),
+			default => throw new \RuntimeException('Invalid attack type given')
+		};
+	}
+
+	private function createArmorType(): ArmorType {
+		return match ($this->armorType) {
+			'ice_block' => new IceBlockType(),
+			'shield' => new ShieldType(),
+			'leather_armor' => new LeatherArmorType(),
+			default => throw new \RuntimeException('Invalid armor type given')
+		};
 	}
 }
