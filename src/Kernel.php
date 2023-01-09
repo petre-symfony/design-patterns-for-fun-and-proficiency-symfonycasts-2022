1d1a8ac9800162cc7ab927bbbbc9eb66dbf08099
petrero <petrero@gmail.com>
2023-01-09 09:29:01 +0200

    9.2 Setting up Autoconfiguration  -The downside to this solution is that every time we add a new observer, we'll need to go to services.yaml and wire it manually. Gasp, how undignified...
     Could we automatically subscribe all services that implement GameObserverInterface? Why, yes! And what an excellent idea! We can do that in two steps.
    
     First, open src/Kernel.php. This isn't a file we work with much, but we're about to do some deeper things with the container and so this is exactly where we want to be. Go to Code Generate or Command+O and select "Override Methods". We're going to override one called build()
    
     Perfect! The parent method is empty, so we don't need to call it at all. Instead, say $container->registerForAutoconfiguration(), pass it GameObserverInterface::class, and then say ->addTag(). I'm going to invent a new tag here called game.observer
    
     This probably isn't something you see very often (or ever) in your code, but it's really common in third-party bundles. This says that any service that implements GameObserverInterface should automatically be given this game.observer tag... assuming that service has autoconfigure enabled, which all of our services do.
    
     That tag name could be any string... and it doesn't do anything at the moment: it's just a random string that's now attached to our service.
    
     But we should, at least, be able to see it. Spin over and run:
    
      ./bin/console debug:container xpearnedobserver
     It found our service! And check it out: Tags - game.observer.

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,9 +2,19 @@
 
 namespace App;
 
+use App\Observer\GameObserverInterface;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel {
 	use MicroKernelTrait;
+
+	protected function build(ContainerBuilder $container) {
+		$container
+			->registerForAutoconfiguration(GameObserverInterface::class)
+			->addTag('game.observer');
+	}
+
+
 }
